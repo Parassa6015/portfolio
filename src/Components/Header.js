482e0875358649da5d02fdf65695cbf762941369
Parassa6015 <parassa1006@gmail.com>
2025-06-17 17:25:44 -0700

    Will add project button later

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -9,7 +9,7 @@ const Header1 = () => {
         <nav style={{ width: '400px' }}>
             <ul className="nav-list">
                 <li><a href="#about">About</a></li>
-                <li><a href="#projects">Projects</a></li>
+                {/* <li><a href="#projects">Projects</a></li> */}
                 <li><a href="#skills">Skills</a></li>
                 <li><a href="#contact">Contact</a></li>
             </ul>
